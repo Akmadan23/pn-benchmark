3dcabb1699e3446b395c3a84a2c2783756b949c9
Akmadan23 <azadahmadi@mailo.com>
2022-10-22 22:28:53 +0200

    Add `lisp`

---
+++ lisp/pn.lisp
@@ -0,0 +1,20 @@
+#!/usr/bin/sbcl --script
+
+(setq p 1)
+(setq n 100000)
+(print 2)
+(print 3)
+
+(loop :for i :from 5 :to n :by 2 do
+    (loop :for j :from 3 :to (/ i 2) :by 2 do
+        (if (= (mod i j) 0)
+            (progn
+                (setq p 0)
+                (return))
+        ; else
+            (setq p 1)))
+
+    (if (= p 1)
+        (progn
+            (print i)
+            (setq p 0))))
